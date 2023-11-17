9dc38929a569fa17e4ac767ec23e3f4e86e9aa87
Vince Peo <48032663+Vin7ag3@users.noreply.github.com>
2023-11-17 15:33:37 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,24 @@ const Footer = () => {
         <div>
           <a
             href='https://github.com/Vin7ag3'
+            onMouseOver={({target})=>target.style.color="8b0000"}
+            onMouseOut={({target})=>target.style.color="d5ceca"}
             className='fticon'
           >
             <FaGithub size={24} />
           </a>
           <a
             href='https://www.linkedin.com/in/vincepeo'
+            onMouseOver={({target})=>target.style.color="8b0000"}
+            onMouseOut={({target})=>target.style.color="d5ceca"}
             className='fticon'
           >
             <FaLinkedin size={24} />
           </a>
           <a
             href='https://stackoverflow.com/users/22928987/vince-peo'
+            onMouseOver={({target})=>target.style.color="8b0000"}
+            onMouseOut={({target})=>target.style.color="d5ceca"}
             className='fticon'
           >
             <FaStackOverflow size={24} />
